59ba01b118cd70e800fd6e6355b264c5b128876f
Trung0246 <trung0246@gmail.com>
2016-12-06 23:36:16 -0800

    Update Shmup.new.js

--- build/Shmup.new.js
+++ build/Shmup.new.js
@@ -34,51 +34,61 @@
         maxGun: 0,
         maxProcess: 0,
       },
-      gun = {},                 //Gun holder
-      projectile = {},          //Bullet holder
       pool = {                  //Pool holder
         gun: undefined,
         process: undefined,
       },
       process = {               //Process holder, use for bullet left in playground when gun deleted
         active: [],
-        wait: [],
+        wait: {},
       },
       temp = {                //Temp holder
         repeat: {},
       };
   
   //Projectile classes
-  projectile.bullet = {
+  main.projectile = {};
+  
+  main.projectile.bullet = {
     data: {
       position: {
         x: 0,
         y: 0,
       },
-      angle: {
-        value: 0,
-        temp: 0,
-        sin: 0,
-        cos: 0,
-      },
+      angle: 0,
       speed: 0,
-      change: {
-        angle: {
-          value: 0,
-          times: 0,
-        },
-        speed: {
-          value: 0,
-          times: 0,
-        },
-      },
     },
-    create: function(gunData, projectile, data) {
-      
+    create: function(data) {
+      //debugger;
+      var projectile = process.wait.bullet.get();
+      if (data.data) {
+        projectile.data = JSON.parse(JSON.stringify(data.data));
+      }
+      projectile.position.x = data.position.x || 0;
+      projectile.position.y = data.position.y || 0;
+      projectile.angle = data.angle || 0;
+      projectile.speed = data.speed || 0;
+      projectile.update = data.update;
+      if (data.process) {
+        projectile.process.actions = Shmup.util.loop(data.process.condition, data.process.actions, false, projectile);
+      }
+      process.active.push(projectile);
       return projectile;
     },
-    update: function(bullet) {
-      
+    vanish: function(projectile) {
+      projectile.position.x = 0;
+      projectile.position.y = 0;
+      projectile.angle = 0;
+      projectile.speed = 0;
+      projectile.data = undefined;
+      projectile.update = undefined;
+      projectile.process.actions = undefined;
+      projectile.process.temp = [1337];
+      process.wait.bullet.set(projectile);
+    },
+    update: function(projectile) {
+      projectile.position.x = Math.sin(projectile.angle) * projectile.speed + projectile.position.x;
+      projectile.position.y = Math.cos(projectile.angle) * projectile.speed + projectile.position.y;
     },
   };
   
@@ -88,119 +98,50 @@
     main.maxProjectile = configsData.maxProjectile || (function() {
       throw new Error("Unknown maxProjectile");
     })();
-    main.maxGun = configsData.maxGun || (function() {
-      throw new Error("Unknown maxGun");
-    })();
-    main.maxProcess = configsData.maxProcess || (function() {
-      throw new Error("Unknown maxProcess");
-    })();
-    pool.gun = new Pool({
-      position: {
-        x: 0,
-        y: 0,
-      },
-      angle: 0,
-      projectile: undefined,
-      data: undefined,
-    }, configsData.maxGun);
-    pool.process = new Pool({
-      type: undefined,
-      location: undefined,
-      condition: undefined,
-      actions: undefined,
-    }, configsData.maxProcess, true);
-    for (var key in projectile) {
-      if (!projectile[key].data.process) {
-        projectile[key].data.process = [1337]; //1337 act as a flag for run to check when run done
+    for (var key in main.projectile) {
+      if (!main.projectile[key].data.process) {
+        main.projectile[key].data.process = {
+          temp: [1337], //1337 act as a flag for run to check when run done
+          actions: undefined, //Actions function holder use with main.util.loop
+        };
       }
-      if (!projectile[key].data.data) {
-        projectile[key].data.data = {};
+      if (!main.projectile[key].data.data) {
+        main.projectile[key].data.data = undefined;
+      }
+      if (!main.projectile[key].data.type) {
+        main.projectile[key].data.type = key;
+      }
+      if (!main.projectile[key].data.update) {
+        main.projectile[key].data.update = undefined;
+      }
+      if (!main.projectile[key].temp) {
+        main.projectile[key].temp = {};
+      }
+      //debugger;
+      if (!process.wait[key]) {
+        process.wait[key] = new Pool(main.projectile[key].data, main.maxProjectile, true);
       }
     }
     return;
   };
   main.update = function() {
-    
-  };
-  
-  //Gun class
-  main.gun = {
-    create: function(data) {
-      if (!gun[data.name]) {
-        gun[data.name] = pool.gun.get();
-        //Number
-        gun[data.name].position.x = data.position.x || 0;
-        gun[data.name].position.y = data.position.y || 0;
-        gun[data.name].angle = data.angle || 0;
-        //Projectile
-        gun[data.name].projectile = {
-          update: data.projectile.update,
-          type: data.projectile.type,
-          active: [],
-          wait: [],
-        };
-        //Data
-        gun[data.name].data = data.data;
-        //Other
-        return gun[data.name];
+    for (var projectileCount = process.active.length - 1; projectileCount >= 0; --projectileCount) {
+      if (process.active[projectileCount].update === undefined) {
+        process.active.splice(projectileCount, 1);
       } else {
-        throw new Error("Gun " + data.name + " was already defined");
-      }
-    },
-    erase: function(name) {
-      if (gun[name]) {
-        //Number
-        gun[name].position.x = 0;
-        gun[name].position.y = 0;
-        gun[name].angle = 0;
-        //Projectile
-        if (gun[name].projectile.active.length <= 0) {
-          process.wait.push(gun[name].projectile.active);
-        } else {
-          process.active.push(gun[name].projectile.active);
+        if (process.active[projectileCount].process.actions) {
+          process.active[projectileCount].process.actions();
         }
-        process.wait.push(gun[name].projectile.wait);
-        gun[name].projectile = undefined;
-        //Data
-        gun[name].data = {};
-        //Other
-        pool.gun.set(gun[name]);
-        delete gun[name];
-        return true;
-      } else {
-        throw new Error("Gun " + name + " was not defined");
+        main.projectile[process.active[projectileCount].type].update(process.active[projectileCount]);
+        process.active[projectileCount].update(process.active[projectileCount]);
       }
-    },
-    data: function(name) {
-      if (gun[name]) {
-        return gun[name];
-      } else {
-        throw new Error("Gun " + name + " was not defined");
-      }
-    },
+    }
   };
   
   //Utilities class
   main.util = {
-    fire: function(gunData, data) {
-      if (gunData.projectile.wait.length > 0) {
-        gunData.projectile.active.push(gunData.projectile.wait[gunData.projectile.wait.length - 1]);
-        gunData.projectile.wait.pop();
-      } else {
-        gunData.projectile.active.push(projectile[gunData.projectile.type].data);
-      }
-      var tempProjectile = projectile[gunData.projectile.type].create(gunData, gunData.projectile.active[gunData.projectile.active.length - 1], data);
-      //tempProjectile.process.push(pool.process.get());
-      return tempProjectile;
-    },
-    run: function (condition, actions, isMain, repeatData) {
+    loop: function (condition, actions, isMain, repeatData) {
       if (isMain === undefined) {
-        /*var tempRepeat = pool.process.get();
-        tempRepeat.type = "repeat";
-        tempRepeat.location = 0;
-        tempRepeat.condition = condition;
-        tempRepeat.actions = actions;
-        return tempRepeat;*/
         return {
           type: "repeat",
           location: 0,
@@ -214,12 +155,9 @@
       }
       function mainProcess() {
         var tempData, tempBool, tempProcess;
+        //debugger;
         if (typeof repeatData === "string") {
           if (!temp.repeat[repeatData]) {
-            /*temp.repeat[repeatData] = [pool.process.get()];
-            temp.repeat[repeatData][0].location = 0;
-            temp.repeat[repeatData][0].condition = condition;
-            temp.repeat[repeatData][0].actions = actions;*/
             temp.repeat[repeatData] = [{
               location: 0,
               condition: condition,
@@ -228,28 +166,24 @@
           }
           tempProcess = temp.repeat[repeatData];
         } else if (typeof repeatData === "object") {
-          if (typeof repeatData.process[0] === 1337) {
-            repeatData.process.pop();
-            /*repeatData.process.push(pool.process.get());
-            repeatData.process[0].location = 0;
-            repeatData.process[0].condition = condition;
-            repeatData.process[0].actions = actions;*/
-            repeatData.process.puah({
+          if (repeatData.process.temp[0] === 1337) {
+            repeatData.process.temp.pop();
+            repeatData.process.temp.push({
               location: 0,
               condition: condition,
               actions: actions,
             });
           }
-          tempProcess = repeatData.process;
+          tempProcess = repeatData.process.temp;
         }
         if (tempProcess[tempProcess.length - 1].type === "wait") {
-          if (tempProcess[tempProcess.length - 1].condition()) {
+          if (tempProcess[tempProcess.length - 1].condition(repeatData, tempProcess[tempProcess.length - 1])) {
             return;
           } else {
             tempProcess.pop();
           }
         }
-        tempBool = tempProcess[tempProcess.length - 1].condition() || (tempProcess[tempProcess.length - 1].location < tempProcess[tempProcess.length - 1].actions.length && tempProcess[tempProcess.length - 1].location !== 0);
+        tempBool = tempProcess[tempProcess.length - 1].condition(repeatData, tempProcess[tempProcess.length - 1]) || (tempProcess[tempProcess.length - 1].location < tempProcess[tempProcess.length - 1].actions.length && tempProcess[tempProcess.length - 1].location !== 0);
         while (tempBool) {
           while (tempProcess[tempProcess.length - 1].location < tempProcess[tempProcess.length - 1].actions.length) {
             if (typeof tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location] === "object") {
@@ -260,7 +194,7 @@
                 tempProcess.push(tempData);
                 break;
               } else if (tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location].type === "wait") {
-                if (tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location].condition()) {
+                if (tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location].condition(repeatData, tempProcess[tempProcess.length - 1])) {
                   tempData = tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location];
                   tempProcess[tempProcess.length - 1].location += 1;
                   tempProcess.push(tempData);
@@ -273,37 +207,37 @@
                     };
                     break;
                   }
-                  tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location]();
+                  tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location](repeatData);
                 }
               } else {
                 throw new Error("Invalid action at " + tempProcess[tempProcess.length - 1].location);
               }
             } else {
-              tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location]();
+              tempProcess[tempProcess.length - 1].actions[tempProcess[tempProcess.length - 1].location](repeatData);
             }
             tempProcess[tempProcess.length - 1].location += 1;
           }
           if (tempData) {
             break;
           }
-          tempBool = tempProcess[tempProcess.length - 1].condition();
+          tempBool = tempProcess[tempProcess.length - 1].condition(repeatData, tempProcess[tempProcess.length - 1]);
           tempProcess[tempProcess.length - 1].location = 0;
         }
         if (tempData) {
           if (tempData.type === "repeat") {
-            main.util.run(condition, actions, true, repeatData);
+            main.util.loop(condition, actions, true, repeatData);
           } else if (tempData.type === "wait") {
             return;
           }
         } else {
           tempProcess.pop();
           if (tempProcess.length > 0) {
-            main.util.run(condition, actions, true, repeatData);
+            main.util.loop(condition, actions, true, repeatData);
           } else {
             if (typeof repeatData === "string") {
               delete temp.repeat[repeatData];
             } else {
-              repeatData.process.push(1337);
+              repeatData.process.temp.push(1337);
             }
           }
         }
@@ -314,44 +248,86 @@
         type: "wait",
         condition: condition,
       };
-      /*var tempData = pool.process.get();
-      tempData.type = "wait";
-      tempData.condition = condition;
-      return tempData;*/
     },
     clear: function(all) {
       
     },
     debug: {
-      
+      debugMode: function() {
+        debugger;
+      },
     },
   };
   
   //Math class
   main.math = {
-    oval: function(horizontal, vertical, fireAngle, spinAngle) {
-      
-    },
-    radToDeg: function(angle) {
-      
-    },
-    degToRad: function(angle) {
-      
-    },
-    normalizeRadian: function(angle) {
-      
-    },
-    normalizeDegree: function(angle) {
-      
+    oval: function(position, horizontal, vertical, fireAngle, spinAngle) {
+      var tempPos = {};
+      tempPos.x = position.x + horizontal * Math.cos(fireAngle) * Math.cos(spinAngle) - vertical * Math.sin(fireAngle) * Math.sin(spinAngle);
+      tempPos.y = position.y + vertical * Math.sin(fireAngle) * Math.cos(spinAngle) + horizontal * Math.cos(fireAngle) * Math.sin(spinAngle);
+      return {
+        angle: main.math.aim(position, tempPos),
+        speed: main.math.pythagorean(position, tempPos),
+      };
     },
     pythagorean: function(locationOne, locationTwo) {
-      
+      return Math.sqrt(Math.pow(locationOne.x - locationTwo.x, 2) + Math.pow(locationOne.y - locationTwo.y, 2));
     },
     aim: function(start, target) {
-      
+      return -Math.atan2(start.x - target.x, -(start.y - target.y));
     },
-    interpolate: function() {
-      
+    interpolation: {
+      //what the fuck is frame / time ?
+      //start and end use x with x and y with y
+      linear: function(start, end, time) {
+        return start + time * (end-start);
+      },
+      smoothStep: function(start, end, time) {
+        return start + Math.pow(time, 2) * (3 - 2 * time) * (end - start);
+      },
+      smootherStep: function(start, end, time) {
+        return start + Math.pow(time, 3) * (time * (time * 6 - 15) + 10) * (end - start);
+      },
+      acceleration: function(start, end, time) {
+        var temp = Math.pow(time, 2);
+        return (end * temp) + (start * (1 - temp));
+      },
+      deceleration: function(start, end, time) {
+        var temp = 1 - (1 - time) * (1 - time);
+        return (end * temp) + (start * (1 - temp));
+      },
+      overShoot: function(start, end, time, magnitude) {
+        time = main.math.interpolation.deceleration(0, 1, time);
+        return start + time * (end-start) * (1 + Math.sin(time * 180) * magnitude);
+      },
+    },
+    radToDeg: function(radian) {
+      radian = radian % (Math.PI * 2);
+      return radian * (180 / Math.PI);
+    },
+    degToRad: function(degree) {
+      degree = degree % 360;
+      return degree * (Math.PI / 180);
+    },
+    normalizeRadian: function(radian) {
+        radian = radian % (Math.PI * 2);
+      while (radian <= -Math.PI) {
+        radian += Math.PI * 2;
+      }
+      while (Math.PI < radian) {
+        radian -= Math.PI * 2;
+      }
+      return radian;
+    },
+    normalizeDegree: function(degree) {
+      degree = degree % 360;
+      while (degree <= 0) {
+        degree += 360;
+      }
+      while (180 < degree) {
+        degree -= 360;
+      }
+      return degree;
     },
   };
   
